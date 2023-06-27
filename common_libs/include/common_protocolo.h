29ec6c23190ce11e91213f75384df50d6c3b3e5b
santipira <spiragine@fi.uba.ar>
2023-06-27 10:30:29 -0300

    feat: Server y Cliente
    Server: Se deja funcionando Sender Recieve / Crear y Unirse a partidas / Lanza Thread Juego / Pushea en Queue y Popea Eventos de usuario (MOVE/STOP MOVE/ SHOOT/ STOP SHOOT)
    
    Cliente: Se reestructura carpetas, se agrega configuracion basica de Sender y Recieve que se lanzan en el hilo main.
    
    Common: Se avanza en protocolo, ya con funcionalidades para send/recieve tanto en Server como en Cliente.

---
+++ common_libs/include/common_protocolo.h
@@ -0,0 +1,54 @@
+#ifndef COMMON_PROTOCOLO_H_
+#define COMMON_PROTOCOLO_H_
+
+#include <cstdint>
+#include <string>
+#include <vector>
+#include "common_socket.h"
+#include "common_message.h"
+#include "eventos/evento.h"
+#include <cstring>
+#include <iostream>
+#include <netinet/in.h>
+#include <functional>
+#include "common_mensaje_cliente.h"
+#include "eventos/creador_eventos.h"
+#include "evento_usuario.h"
+
+#define CODIGO_INVALIDO 0xFFFFFFFF
+
+class Protocolo {
+ private:
+    Socket socket;
+    bool wasClosed;
+    void sendByte(uint8_t byte);
+    void sendCuatroBytes(int32_t param1);
+    int sendData(const std::string& data);
+
+    uint8_t recvByte();
+    uint16_t recvDosBytes();
+    uint32_t recvCuatroBytes();
+    Message getMessage(std::string& estadoJuego);
+
+ public:
+    Protocolo(const std::string &hostname, const std::string &servname);
+    explicit Protocolo(Socket socket);
+
+    void enviar_codigo_partida(uint32_t codigoPartida);
+    void enviar_estado_juego(std::string estado);
+    void enviar_inicio_partida(std::vector<std::string> inicioPartida);
+    void enviar_info(std::string info, std::string inicio);
+    void enviar_evento(const EventoUsuario &eventoUsuario);
+
+    Message recibir_estado_juego();
+    std::string recibir_inicio_partida();
+    Evento *recibir_evento(int& idPersonaje);
+    Personaje recibir_personaje();
+    uint32_t recibir_codigo_partida();
+    uint8_t recibir_modo();
+    std::string recibir_nombre();
+    void cerrarConexion();
+    ~Protocolo();
+
+};
+#endif // COMMON_PROTOCOLO_H_
